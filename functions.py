56149d93c3eeb9d27902b87583e3e6a7992aea0c
Richard <r50206v@gmail.com>
2019-04-22 18:52:42 +0800

    [FEATURE] adding boxcox transformation

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
+import sys
 import numpy as np
 import pandas as pd
+import scipy.stats as stats
 
 
 def getER(df, n):
@@ -40,3 +42,30 @@ def getBlock(y, p):
             )
         blockList.append(rowList)
     return np.block(blockList)
+
+
+def _shift(df):
+    constant = df[df > 0].mean().values[0]
+    if all(df < 0):
+        constant = 1
+    if df.min().values[0] <= 0:
+        df = df + (-1)*df.min().values[0] + constant
+    return df
+
+
+def boxcox(df):
+    df = _shift(df)
+    try:
+        y, lam, ci = stats.boxcox(df, alpha=0.05)
+    except RuntimeError: 
+        # when boxcox cannot converge
+        sys.stderr.write('[WARNING] BoxCox cannot converge\n')
+        return np.log(df)
+    
+    if (ci[0] > lam) or (ci[1] < lam):
+        # if the lambda is out of the confidence interval
+        # return constant value 
+        # (`get` will use logarithm as an alternative)
+        sys.stderr.write('[WARNING] lambda not in confidence interval\nlambda: {0}, CI: {1}\n'.format(lam, ci))
+        return np.log(df)
+    return pd.DataFrame(y, index=df.index, columns=df.columns)
